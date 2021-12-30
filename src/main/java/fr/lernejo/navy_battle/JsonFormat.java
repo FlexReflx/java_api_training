dea90820232e9f3ac9ee42c1e68ec68c02a76ba2
Leo Bendref <leo.bendref@gmail.com>
2021-12-30 16:00:19 +0100

    part3 evolution
    test
    
    modifies
    
    modif test

--- src/main/java/fr/lernejo/navy_battle/JsonFormat.java
+++ src/main/java/fr/lernejo/navy_battle/JsonFormat.java
@@ -1,2 +1,23 @@
-package fr.lernejo.navy_battle;public class JsonFormat {
+package fr.lernejo.navy_battle;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class JsonFormat {
+    final String id;
+    final String url;
+    final String message;
+    public JsonFormat(String id,String url,String m){
+        this.id = UUID.randomUUID().toString();
+        this.url = url;
+        this.message = m;
+    }
+    public String ToString(){
+        Map<String,String> returnObject = new HashMap<>();
+        returnObject.put("id",this.id);
+        returnObject.put("url",String.valueOf(this.url));
+        returnObject.put("message",this.message);
+        return "";
+    }
 }
--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,4 +1,4 @@
-package navy_battle;
+package fr.lernejo.navy_battle;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -10,21 +10,50 @@ import java.net.InetSocketAddress;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import static java.lang.Integer.parseInt;
+
 public class Launcher {
+    static class MyHandler implements HttpHandler {
+
+        private final String RESPONSE = "OK";
+
+        public void handle(HttpExchange t) throws IOException {
+            t.sendResponseHeaders(200, RESPONSE.length());
+            OutputStream os = t.getResponseBody();
+            os.write(RESPONSE.getBytes());
+            os.close();
+        }
+    }
+    static class JsonHandler implements HttpHandler {
+
+        private final String RESPONSE = "OK";
+
+        public void handle(HttpExchange t) throws IOException {
+            t.sendResponseHeaders(200, RESPONSE.length());
+            OutputStream os = t.getResponseBody();
+            os.write(RESPONSE.getBytes());
+            os.close();
+        }
+    }
+
     public static void main(String[] args) throws IOException {
-        HttpServer serv = HttpServer.create(new InetSocketAddress(8000), 0);
-        ExecutorService es = Executors.newFixedThreadPool(1);
-        serv.createContext("/ping", new MyHandler());
-        serv.start();
+        System.out.println(args.length);
+        if (args.length > 0) {
+            if (args.length == 1) {
+                HttpServer serv = HttpServer.create(new InetSocketAddress(parseInt(args[0])), 0);
+                ExecutorService es = Executors.newFixedThreadPool(1);
+                serv.createContext("/ping", new MyHandler());
+                serv.createContext("/api/game/start", new JsonHandler());
+                System.out.println("heeeee");
+                serv.start();
+            }
+
+
+        }else{
+            System.out.println("aucun port passé en argument");
+        }
     }
 }
-class MyHandler implements HttpHandler {
-    public void handle(HttpExchange t) throws IOException {
-        byte [] response = "Welcome Real's HowTo test page".getBytes();
-        t.sendResponseHeaders(200, response.length);
-        OutputStream os = t.getResponseBody();
-        os.write(response);
-        os.close();
-    }
-}
+
+
 
